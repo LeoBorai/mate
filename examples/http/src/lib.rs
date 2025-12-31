3bfaf611856c2322c20989909b781d1d00b33e2e
Leo Borai <estebanborai@gmail.com>
2025-12-31 20:33:38 +0100

    feat: use wstd to run http/async code

--- examples/http/src/lib.rs
+++ examples/http/src/lib.rs
@@ -1,3 +1,4 @@
+use serde_json::Value;
 use wstd::http::{Body, BodyExt, Client, Method, Request};
 
 use mate_job::{mate_handler, mate_object};
@@ -5,28 +6,30 @@ use mate_job::{mate_handler, mate_object};
 #[mate_object]
 struct Config {
     api_url: String,
-    data: serde_json::Value,
+    data: Value,
 }
 
 #[mate_object]
 struct Response {
     status: u16,
-    body: serde_json::Value,
+    body: Value,
 }
 
 #[mate_handler]
 async fn send_http_request(config: Config) -> Result<Response> {
-    let client = Client::new();
-    let mut request = Request::builder();
-    request = request.uri(config.api_url).method(Method::POST);
+    wstd::runtime::block_on(async move {
+        let client = Client::new();
+        let mut request = Request::builder();
+        request = request.uri(config.api_url).method(Method::POST);
 
-    let body = Body::from_json(&config.data).expect("Bad body");
-    let request = request.body(body)?;
-    let response = client.send(request).await?;
-    let status = response.status().as_u16();
-    let body = response.into_body().into_boxed_body().collect().await?;
-    let bytes = body.to_bytes();
-    let json = serde_json::from_slice::<serde_json::Value>(&bytes)?;
+        let body = Body::from_json(&config.data).expect("Bad body");
+        let request = request.body(body).unwrap();
+        let response = client.send(request).await.unwrap();
+        let status = response.status().as_u16();
+        let body = response.into_body().into_boxed_body().collect().await?;
+        let bytes = body.to_bytes();
+        let json = serde_json::from_slice::<serde_json::Value>(&bytes)?;
 
-    Ok(Response { status, body: json })
+        Ok(Response { status, body: json })
+    })
 }
